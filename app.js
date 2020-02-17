0efc166c19788b8da1293a883dd0362bcd3b08c8
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-02-17 15:38:48 +0700

    remove csvtojson. Clean up srtring from LINE csv file before working with it.

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
+const parse = require('csv-parse');
 const fs = require('fs');
-const csv2json = require('csvtojson');
-const filePath = "/Users/admin/Documents/header.csv";
+const filePath = "/Users/admin/Documents/ohsva.csv";
 
-csv2json().fromFile(filePath).then(source => {
-    console.log(source);
+const output = [];
+const option = {
+    delimiter:',',
+    trim: true,
+    columns:true
+};
+
+console.log();
+console.log('Start app....');
+console.log();
+console.log();
+fs.readFile(filePath,function(err,data){
+    console.log('File content :'); 
+    let input = data.toString();
+    console.log(input);
+    input = input.replace(/\u0000/g,'').replace('��','').replace(/\t/g,',');;
+    console.log()
+    console.log('after clean up');
+    console.log(input);
+    /*
+    let input2 = `"User Id","MID","Display Name","Profile Picture","Crm Id","First Name","Last Name","Thai Citizen Id","Gender","Date Of Birth","Phone Number","Email","Address","Street","Sub District","District","City","Zipcode","Groups","Categories","Beacons","Text Message - ohsva (exact)"
+    "5360540","U2d5f47348dce2859a4de746e5b80eaed","Oh SVA","https://profile.line-scdn.net/0hk9OWPt4sNBZyLh-bC71LQU5rOnsFADJeCkB8I14paXVYGXRETkl4JQApOXRbHSdGGkF9dQcoaiYN","-","-","-","-","-","-","-","-","-","-","-","-","-","-","PLSO EMIT LINE Group, test, MCX","-","-","Message #1: ohsva"`;
+    */
+    
+
+    const parser = parse(input,option);
+
+    parser.on("readable",function(){
+        let record
+        while (record = parser.read()) {
+            console.log('record:');
+            console.log(record);
+            output.push(record)
+        }
+    })
+    .on('error', function(err){
+        console.error(err.message)
+    })
+    .on('end', function(){
+        console.log();
+        console.log();
+        console.log('output:');
+        console.log(output);
+    });
+    
 });
