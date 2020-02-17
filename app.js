989e3d9128ad19f77249ae68fbaa688dafd616de
Admin <somkamol.vasuvudhiroch@exxonmobil.com>
2020-02-17 16:37:30 +0700

    Use encoding ucs2 to remove \u0000 and �� that was put in front, tab stay still. quote is

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const filePath = "/Users/admin/Documents/ohsva.csv";
 
 const output = [];
 const option = {
-    delimiter:',',
+    quote : '',
+    delimiter:'\t',
     trim: true,
+    rtrim : true,
     columns:true
 };
 
@@ -15,18 +17,36 @@ console.log();
 console.log();
 fs.readFile(filePath,function(err,data){
     console.log('File content :'); 
-    let input = data.toString();
+    let input = data.toString('ucs2');
     console.log(input);
+    console.log();
+    console.log();
+    /*
+    let arr = [];
+    let q = [];
+    for (let i = 0; i < input.length; i++) {
+        const e = input[i];
+        console.log(e);
+        arr.push(e);
+        if(e === '\u0000'){
+            q.push(e);    
+        }
+    }
+    console.log(arr);
+    console.log(q);
+    */
+    /*
     input = input.replace(/\u0000/g,'').replace('��','').replace(/\t/g,',');;
     console.log()
     console.log('after clean up');
     console.log(input);
+    */
     /*
     let input2 = `"User Id","MID","Display Name","Profile Picture","Crm Id","First Name","Last Name","Thai Citizen Id","Gender","Date Of Birth","Phone Number","Email","Address","Street","Sub District","District","City","Zipcode","Groups","Categories","Beacons","Text Message - ohsva (exact)"
     "5360540","U2d5f47348dce2859a4de746e5b80eaed","Oh SVA","https://profile.line-scdn.net/0hk9OWPt4sNBZyLh-bC71LQU5rOnsFADJeCkB8I14paXVYGXRETkl4JQApOXRbHSdGGkF9dQcoaiYN","-","-","-","-","-","-","-","-","-","-","-","-","-","-","PLSO EMIT LINE Group, test, MCX","-","-","Message #1: ohsva"`;
     */
     
-
+    
     const parser = parse(input,option);
 
     parser.on("readable",function(){
